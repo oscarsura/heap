e76b3b4a31e20e651ebc1e06508302a37d2975a2
Oscar Sura <surao@stanford.edu>
2018-09-13 20:34:09 -0700

    Resolved compilation errors with driver.

--- driver.cpp
+++ driver.cpp
@@ -5,22 +5,25 @@
  * for testing, and builds the environment to record algorithm statistics.
  */
 
-#include <sys/mman.h> //for mmap, munmap
-#include <assert.h>   //for assert
 #include <iostream>   //for cerr
+#include <assert.h>   //for assert
+#include <sys/mman.h> //for mmap, munmap
+#include "driver.h"
 
 #define HEAP_START_ADDR 0x0 //define heap start at address 0x0
 #define MEM_PROTECTION (PROT_READ|PROT_WRITE) //default memory protection
-#define FLAGS (MAP_PRIVTE|MAP_ANONYMOUS) //privately mapped region, no file
+#define FLAGS (MAP_PRIVATE|MAP_ANONYMOUS) //privately mapped region, no file
+
+using namespace std;
 
 static void* kSegmentStart = nullptr; //default segment address
 static size_t kSegmentSize = 0; //default segment size
 
-void* getSegmentStart {
+void* getSegmentStart() {
     return kSegmentStart;
 }
 
-size_t getHeapSize {
+size_t getHeapSize() {
     return kSegmentSize;
 }
 
@@ -33,21 +36,24 @@ void* initHeapSegment(size_t requestedSize) {
         kSegmentSize = 0;
     }
     kSegmentStart = mmap(HEAP_START_ADDR, requestedSize, MEM_PROTECTION, FLAGS, fd, offset);
-    assert(segment_start != MAP_FAILED);
+    assert(kSegmentStart != MAP_FAILED);
     kSegmentSize = requestedSize;
     return kSegmentStart;
 }
 
 static size_t kSegmentUnmapError = -1;
 void freeHeapSegment(void* segmentStart, size_t segmentSize) {
-    if (munmap((segmentStart, segmentSize) == -1) {
-        cerr << "Could not unmap the region of memory at addr: " << (char*)sgmentStart << endl;
+    if (munmap(segmentStart, segmentSize) == -1) {
+        cerr << "Could not unmap the region of memory at addr: " << (char*)segmentStart << endl;
         exit(kSegmentUnmapError);
     }
 }
 
+static size_t kArgumentMissing = -2;
 static size_t kDefaultHeapSize = (1 << 20);
-int main() {
+int main(int argc, char *argv[]) {
+    if (argc < 3) { 
+    }
     initHeapSegment(kDefaultHeapSize); //mmap
     freeHeapSegment(kSegmentStart, kSegmentSize); //munmap
     return 0;

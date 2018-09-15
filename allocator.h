378cd86569d9f748534ed2a60078fe09735954d8
Oscar Sura <surao@stanford.edu>
2018-09-14 20:53:54 -0700

    Introduced allocator framework.

---
+++ allocator.h
@@ -0,0 +1,19 @@
+/*
+ * File: allocator.h
+ * -----------------
+ */
+
+#ifndef _ALLOCATOR_H_
+#define _ALLOCATOR_H_
+
+bool init();
+
+void malloc();
+
+void realloc();
+
+void free();
+
+bool validateSegment();
+
+#endif
